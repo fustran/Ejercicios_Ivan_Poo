a80145ec5d916306f638b3816a4b0a8791552364
IVÁN FUSTER SIMARRO <ifussim@outlook.com>
2025-01-22 00:19:19 +0100

    Comentarios

--- src/main/java/com/poo/ClasePersona/Persona.java
+++ src/main/java/com/poo/ClasePersona/Persona.java
@@ -1,3 +1,5 @@
+// Programación Orientada a Objetos. Clase Persona.
+
 package com.poo.ClasePersona;
 
 public class Persona {
@@ -8,16 +10,18 @@ public class Persona {
     private char sexo;
     private int edad;
 
+    // Constructor por defecto
     public Persona(){
 
     }
 
+    // Constructor con parámetros
     public Persona(String nombre, String apellido, String dni, char sexo, int edad){
 
         if (!nombre.equalsIgnoreCase("messi")) { 
-            this.nombre = nombre; // Validación directa
+            this.nombre = nombre;
         } else {
-            this.nombre = "Nombre no válido"; // Valor predeterminado
+            this.nombre = "Nombre no válido";
         }
         this.apellido = apellido;
         this.dni = dni;
@@ -26,52 +30,64 @@ public class Persona {
 
     }
 
+    // Métodos getter y setter
     public String getNombre(){
         return this.nombre;
     }
 
+    // Método setter con validación
     public void setNombre(String nombre){
         if (!nombre.equalsIgnoreCase("messi")){
             this.nombre = nombre;
         }
     }
 
+    // Método getter
     public String getApellido(){
         return this.apellido;
     }
 
+    // Método setter
     public void setApellido(String apellido){
         this.apellido = apellido;
     }
 
+    // Método getter
     public String getDni(){
         return this.dni;
     }
 
+    // Método setter
     public void setDni(String dni){
         this.dni = dni;
     }
 
+    // Método getter
     public char getSexo(){
         return this.sexo;
     }
 
+    // Método setter
     public void setSexo(char sexo){
         this.sexo = sexo;
     }
 
+    // Método getter
     public int getEdad(){
         return this.edad;
     }
 
+    // Método setter
     public void setEdad(int edad){
         this.edad = edad;
     }
 
+    // Método para concatenar nombre y apellido
     public String concatenar(){
         return this.nombre + " " + this.apellido;
     }
 
+    // Método para mostrar información
     public void mostrarInfo(){
         System.out.println(this.nombre + " " + this.apellido + " " + this.dni + " " + this.sexo + " " + this.edad);
 
