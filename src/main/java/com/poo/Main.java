b2ca8d0337ec6f539f44c7f7d4dc85e8842f206c
IVÁN FUSTER SIMARRO <ifussim@outlook.com>
2025-01-21 17:20:26 +0100

    Empezando

--- src/main/java/com/poo/Main.java
+++ src/main/java/com/poo/Main.java
@@ -1,8 +1,25 @@
 package com.poo;
 
+import com.poo.ClasePersona.Persona;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+
+        Persona persona1 = new Persona();
+        Persona persona2 = new Persona("Paco","Ruiz","12345678A",'H',45);
+
+        persona1.mostrarInfo();
+        persona2.mostrarInfo();
+        System.out.println(persona2.concatenar());
+
+        System.out.println("La edad de " +  persona2.getNombre() + " es " +  persona2.getEdad());
+        persona1.setApellido("Sin Nombre");
+        System.out.println(persona1.getApellido());
+        persona1.setNombre("Messi");
+        System.out.println(persona1.getNombre());
+
+        Persona persona3 = new Persona("messi","Ruiz","12345678A",'H',45);
+        persona3.mostrarInfo();
+
     }
 }
