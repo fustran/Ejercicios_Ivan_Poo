0da3a8753f226eedd713d80d1faab4542f1b30fb
IVÁN FUSTER SIMARRO <ifussim@outlook.com>
2025-02-06 17:05:59 +0100

    ...

--- src/main/java/com/poo/Practica_1/AppProgramas.java
+++ src/main/java/com/poo/Practica_1/AppProgramas.java
@@ -1,7 +1,5 @@
 package com.poo.Practica_1;
 
-import java.lang.reflect.Array;
-
 public class AppProgramas {
 
     public static void main (String[] args){
@@ -16,9 +14,9 @@ public class AppProgramas {
         System.out.println(antena3);
         System.out.println();
 
-        /*Cadena Tv1 = new Cadena("Tv1");
+        Cadena Tv1 = new Cadena("Tv1");
         System.out.println(Tv1);
-        System.out.println();*/
+        System.out.println();
 
 
         // Creamos 2 programas.
@@ -31,10 +29,10 @@ public class AppProgramas {
 
         // Programa 2:
         Programa La_Revuelta = new Programa("La Revuelta", antena3, "director1");
-        /*System.out.println(La_Revuelta);
+        System.out.println(La_Revuelta);
         System.out.println();
         System.out.println(Tv1);
-        System.out.println();*/
+        System.out.println();
 
 
         // Insertamos empleados en los programas.
@@ -59,31 +57,31 @@ public class AppProgramas {
         El_Hormiguero.insertarInvitado("Aitana","cantante",1);
         System.out.println();
 
-        /*El_Hormiguero.insertarInvitado("Will Smith","actor",1);
+        El_Hormiguero.insertarInvitado("Will Smith","actor",1);
         System.out.println();
 
         El_Hormiguero.insertarInvitado("Antonio Banderas","actor",2);
         System.out.println();
 
         El_Hormiguero.insertarInvitado("Matias Prats","periodista",2);
-        System.out.println();*/
+        System.out.println();
 
 
         // Insertamos invitados en el programa 2.
         La_Revuelta.insertarInvitado("Aitana","cantante",1);
         System.out.println();
 
-        /*La_Revuelta.insertarInvitado("Arturo Coello","deportista",1);
+        La_Revuelta.insertarInvitado("Arturo Coello","deportista",1);
         System.out.println();
 
-        /*La_Revuelta.insertarInvitado("Agustín Tapia","deportista",1);
+        La_Revuelta.insertarInvitado("Agustín Tapia","deportista",1);
         System.out.println();
 
         La_Revuelta.insertarInvitado("Antonio Resines","actor",2);
         System.out.println();
 
         La_Revuelta.insertarInvitado("Melodi","cantante",2);
-        System.out.println();*/
+        System.out.println();
 
 
         // ver invitados del programa.
@@ -94,11 +92,11 @@ public class AppProgramas {
 
         // b) Ver invitados del programa de una temporada concreta.
         El_Hormiguero.invitadosTemporada(1);
-       /* El_Hormiguero.invitadosTemporada(2);*/
+        El_Hormiguero.invitadosTemporada(2);
 
         La_Revuelta.invitadosTemporada(1);
         System.out.println();
-        /*La_Revuelta.invitadosTemporada(2);*/
+        La_Revuelta.invitadosTemporada(2);
 
 
         // c) Ver la cantidad de veces que ha ido un Invitado al Programa.
@@ -122,6 +120,5 @@ public class AppProgramas {
 
         // f) Usar el metodo implementado en el apartado anterior para que en caso de haber devuelto true buscando en dos Programas distintos, muestre en cuál ha estado antes.
         La_Revuelta.invitadoAntes("Aitana");
-
     }
 }
