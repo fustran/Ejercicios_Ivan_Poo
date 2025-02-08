f79404cfd63075607eb3332fed1f0ee60ac6e007
IVÁN FUSTER SIMARRO <ifussim@outlook.com>
2025-02-08 02:16:42 +0100

    Lombok

--- src/main/java/com/poo/PruebaLombok/AppGestion.java
+++ src/main/java/com/poo/PruebaLombok/AppGestion.java
@@ -1,42 +1,43 @@
 package com.poo.PruebaLombok;
 
-import java.util.ArrayList;
-
 public class AppGestion {
     public static void main(String[] args) {
 
-        Instituto instituto = new Instituto("IES MUTXAMIEL", "Alicante", new ArrayList<>(), new ArrayList<>());
+        Instituto instituto = new Instituto(null,"Alicante");
         instituto.getNombre();
-        
+        /*instituto.setNombre();*/
+
         //Crear cursos
         Curso cursoJava = new Curso("Java", 100);
         System.out.println(cursoJava);
+
         Curso cursoPython = new Curso("Python", 70);
         System.out.println(cursoPython);
+
         instituto.agregarCurso(cursoJava);
         instituto.agregarCurso(cursoPython);
+        instituto.agregarCurso(cursoJava);
+
+        Estudiante estudiante1 = new Estudiante("Carlos", 20, cursoJava);
+        instituto.agregarEstudiante(estudiante1);
+
+        Estudiante estudiante2 = new Estudiante("Ana", 22, cursoPython);
+        instituto.agregarEstudiante(estudiante2);
 
 
-        //crear estudiantes
-       try {
-            Estudiante estudiante1 = new Estudiante("Carlos", 20, cursoJava);
-            Estudiante estudiante2 = new Estudiante("Ana", 22, cursoPython);
-            instituto.agregarEstudiante(estudiante1);
-            instituto.agregarEstudiante(estudiante2);
-
-            //intento de crear un estudiante con nombre nulo (esto lanza NullPointerException)
-            Estudiante estudianteErroneo = new Estudiante(null, 20, cursoJava);
-            System.out.println(estudianteErroneo);
-
-        } catch (NullPointerException e) {
-            System.out.println("No se puede agregar estudiante con nombre nulo");
+        //Añadir un estudiante nulo en la lista de estudiantes
+        try {
+            instituto.agregarEstudiante(null);
+        }catch (NullPointerException e) {
+            System.out.println("Error: Estudiante no puede ser Nulo");
         }
 
-
-        //intento de añadir un estudiante nulo en la lista de estudiantes
-        instituto.agregarEstudiante(null);
-        //intento de añadir un curso nulo en la lista de cursos
-        instituto.agregarCurso(null);
+        //Añadir un curso nulo en la lista de cursos
+        try {
+            instituto.agregarCurso(null);
+        }catch (NullPointerException e) {
+            System.out.println("Error: Curso no puede ser Nulo");
+        }
 
         //mostrar cursos
         System.out.println("Cursos disponibles:");
@@ -45,6 +46,5 @@ public class AppGestion {
         //mostrar estudiantes
         System.out.println("Estudiantes registrados:");
         System.out.println(instituto.getListaEstudiantes());
-
     }
 }
--- src/main/java/com/poo/PruebaLombok/Instituto.java
+++ src/main/java/com/poo/PruebaLombok/Instituto.java
@@ -2,27 +2,44 @@ package com.poo.PruebaLombok;
 
 import lombok.*;
 import java.util.ArrayList;
+import java.util.Objects;
 
 @Getter
-@Setter
+@ToString
 @NoArgsConstructor
 @AllArgsConstructor
-@ToString
 public class Instituto {
 
     @NonNull
     private String nombre;
+    @Setter
     private String poblacion;
-    @NonNull
+    @Setter
     private ArrayList<Estudiante> listaEstudiantes;
-    @NonNull
+    @Setter
     private ArrayList<Curso> listaCursos;
 
-    public void agregarEstudiante(Estudiante estudiante) {
+    public Instituto(String nombre, String poblacion) {
+        this.nombre = Objects.requireNonNullElse(nombre, "Instituto sin nombre");
+        this.poblacion = poblacion;
+        this.listaEstudiantes = new ArrayList<>();
+        this.listaCursos = new ArrayList<>();
+    }
+
+
+    public void agregarEstudiante(@NonNull Estudiante estudiante) {
         this.listaEstudiantes.add(estudiante);
     }
 
-    public void agregarCurso(Curso curso) {
+    public void agregarCurso(@NonNull Curso curso) {
+
+        for (Curso cursos : listaCursos) {
+            if (cursos.getNombre().equals(curso.getNombre())) {
+                System.out.println("El curso de " + curso.getNombre() + " no se puede añadir, ya existe.");
+                return;
+            }
+        }
         this.listaCursos.add(curso);
     }
 }
+
