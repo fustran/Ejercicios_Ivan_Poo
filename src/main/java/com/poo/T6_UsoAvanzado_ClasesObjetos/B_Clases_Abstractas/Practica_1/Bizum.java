de303ba487405225adc0134a9e7239ccd4e2d2e3
IVÁN FUSTER SIMARRO <ifussim@outlook.com>
2025-02-17 10:50:17 +0100

    Avanzando algo

--- src/main/java/com/poo/T6_UsoAvanzado_ClasesObjetos/B_Clases_Abstractas/Practica_1/Bizum.java
+++ src/main/java/com/poo/T6_UsoAvanzado_ClasesObjetos/B_Clases_Abstractas/Practica_1/Bizum.java
@@ -21,7 +21,7 @@ public class Bizum extends MetodoPago {
 
     // Generar el pin de 6 cifras de forma aleatoria
     public int generarPin() {
-        return aleatorio.nextInt(000000, 999999);
+        return aleatorio.nextInt(0, 999999);
     }
 
     // Mostrar el pin para usarlo en la clase tienda
@@ -29,17 +29,17 @@ public class Bizum extends MetodoPago {
         System.out.println("Pin generado: " + pin);
     }
 
-    // Validar el pin siendo siempre true, cuando se valide en el metodo validarBizum, lo contrario a este retorno, será false
+    // Validar el pin siendo siempre true, cuando se valide en validarBizum(), lo contrario a este retorno, será false
     public boolean validarPin(int pinRecibido) {
         return pinRecibido == pin;
     }
 
-    // Validar el teléfono siendo siempre true, cuando se valide en el metodo validarBizum, lo contrario a este retorno, será false
+    // Validar el teléfono siendo siempre true, cuando se valide en validarBizum(), lo contrario a este retorno, será false
     public boolean validarTelefono() {
         return telefono.length() == 9;
     }
 
-    // Validación del bizum llamando a los métodos que devuelven true con esa condición
+    // Validación del bizum llamando a validarPin() y validarTelefono()
     public void validarBizum(int pinValido) {
 
         if (validarTelefono() && validarPin(pinValido)) {
