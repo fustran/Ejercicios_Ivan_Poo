ec7b5f608c67d9c264b8fbfc905a5a2b77bf4d8a
IVÁN FUSTER SIMARRO <ifussim@outlook.com>
2025-02-20 20:43:36 +0100

    ...

--- src/main/java/com/poo/T6_UsoAvanzado_ClasesObjetos/C_Interfaces/A_Dispositivos_Inteligentes/AppGestionDispositivos.java
+++ src/main/java/com/poo/T6_UsoAvanzado_ClasesObjetos/C_Interfaces/A_Dispositivos_Inteligentes/AppGestionDispositivos.java
@@ -1,6 +1,7 @@
 package com.poo.T6_UsoAvanzado_ClasesObjetos.C_Interfaces.A_Dispositivos_Inteligentes;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class AppGestionDispositivos {
 
@@ -11,6 +12,41 @@ public class AppGestionDispositivos {
         listaDispositivos.add(new ParlanteInteligente("Alexa"));
         listaDispositivos.add(new AireAcondicionado("LG"));
 
+        // Clase Anónima
+        Dispositivo proyector = new Dispositivo("Proyector") {
+            @Override
+            protected void encender() {
+                if(isEstado()) {
+                    System.out.println("El proyector ya está encendido");
+                }else{
+                    System.out.println("Encendiendo proyector con ajuste automático");
+                    setEstado(true);
+                }
+
+            }
+        };
+
+        ControlRemoto proyector_control = new ControlRemoto() {
+            @Override
+            public void sincronizar() {
+                System.out.println("incronizando proyector con control remoto de presentación...");
+            }
+        };
+
+        Dispositivo horno = new Dispositivo("Balay") {
+            @Override
+            protected void encender() {
+                if (isEstado()) {
+                    System.out.println("El horno ya está encendido");
+                }else{
+                    System.out.println("Calentando horno con ajuste automático de temperatura...");
+                    setEstado(true);
+                }
+            }
+        };
+
+        listaDispositivos.addAll(Arrays.asList(proyector, horno));
+
         System.out.println();
 
         for(Dispositivo dispositivo : listaDispositivos) {
@@ -19,17 +55,13 @@ public class AppGestionDispositivos {
 
             if (dispositivo instanceof ControlRemoto) {
                 ((ControlRemoto) dispositivo).sincronizar();
+            }else if(dispositivo.getNombre().equals("proyector")){
+                proyector_control.sincronizar();
             }
 
             dispositivo.mostrarEstado();
             dispositivo.apagar();
-            
-            dispositivo.apagar();
-
-            dispositivo.encender();
-            dispositivo.apagar();
-
-
         }
+
     }
 }
