fa51b193ede5f2bc776f7c8b07683de80e38d75b
IVÁN FUSTER SIMARRO <ifussim@outlook.com>
2025-02-20 19:08:43 +0100

    Dispositivos Inteligentes

--- src/main/java/com/poo/T6_UsoAvanzado_ClasesObjetos/C_Interfaces/A_Dispositivos_Inteligentes/AppGestionDispositivos.java
+++ src/main/java/com/poo/T6_UsoAvanzado_ClasesObjetos/C_Interfaces/A_Dispositivos_Inteligentes/AppGestionDispositivos.java
@@ -1,15 +1,5 @@
 package com.poo.T6_UsoAvanzado_ClasesObjetos.C_Interfaces.A_Dispositivos_Inteligentes;
 
-/*
-— Crea una lista de dispositivos.
-
-Recorre la lista para:
-    Encender cada dispositivo.
-    Sincronizar en caso de que un dispositivo implemente ControlRemoto.
-    Mostrar el estado de cada dispositivo.
-    Apagar cada dispositivo.
-*/
-
 import java.util.ArrayList;
 
 public class AppGestionDispositivos {
