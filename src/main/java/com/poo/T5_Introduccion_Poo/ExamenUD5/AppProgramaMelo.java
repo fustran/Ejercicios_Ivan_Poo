ee59e31442635888b0e5a6350cc2a5bec5e14ff9
IVÁN FUSTER SIMARRO <ifussim@outlook.com>
2025-03-06 18:41:28 +0100

    Repitiendo el examen para practicas

--- src/main/java/com/poo/T5_Introduccion_Poo/ExamenUD5/AppProgramaMelo.java
+++ src/main/java/com/poo/T5_Introduccion_Poo/ExamenUD5/AppProgramaMelo.java
@@ -3,26 +3,39 @@ package com.poo.T5_Introduccion_Poo.ExamenUD5;
 public class AppProgramaMelo {
     public static void main(String[] args) {
 
-        // Concurso iesmutxamel = new Concurso();
-        // System.out.println(iesmutxamel);
-        // Equipo maquinitas = new Equipo("Los maquinitas");
-        // System.out.println(maquinitas);
-        // iesmutxamel.inscribirEquipo(maquinitas);
-        // System.out.println(iesmutxamel);
-        // maquinitas.insertarMiembros("Daniel","Lucas");
-        // System.out.println(maquinitas);
-        // maquinitas.insertarMiembros("Carla","Eros","Lucía");
-        // Equipo triunfitos = new Equipo("Los triunfitos");
-        // iesmutxamel.inscribirEquipo(triunfitos);
-        // triunfitos.insertarMiembros("Juan","Patri","Alexia");
-        // System.out.println(triunfitos);
-        // maquinitas.setProblemas_resueltos(2);
-        // maquinitas.calcularPuntuacion();
-        // triunfitos.setProblemas_resueltos(3);
-        // triunfitos.calcularPuntuacion();
+        Concurso iesmutxamel = new Concurso();
+        System.out.println(iesmutxamel);
+
+        Equipo maquinitas = new Equipo("Los maquinitas");
+        System.out.println(maquinitas);
+
+        iesmutxamel.inscribirEquipo(maquinitas);
+        System.out.println(iesmutxamel);
+
+        maquinitas.insertarMiembros("Daniel","Lucas");
+        System.out.println(maquinitas);
+
+        maquinitas.insertarMiembros("Carla","Eros","Lucía");
+
+        Equipo triunfitos = new Equipo("Los triunfitos");
+        iesmutxamel.inscribirEquipo(triunfitos);
+
+        triunfitos.insertarMiembros("Juan","Patri","Alexia");
+        System.out.println(triunfitos);
+
+        // Me he quedado por aqui, seguir luego...
+
+        maquinitas.setProblemasResueltos(2);
+        maquinitas.calcularPuntuacion();
+
+        triunfitos.setProblemasResueltos(3);
+        triunfitos.calcularPuntuacion();
+
         // a)
-        // iesmutxamel.imprimirDatosEquipos();
+        iesmutxamel.imprimirDatosEquipos();
+
         // b)
-        // iesmutxamel.equipoGanador();
+        iesmutxamel.equipoGanador();
+
     }
 }
--- src/main/java/com/poo/T5_Introduccion_Poo/ExamenUD5/Equipo.java
+++ src/main/java/com/poo/T5_Introduccion_Poo/ExamenUD5/Equipo.java
@@ -1,4 +1,87 @@
 package com.poo.T5_Introduccion_Poo.ExamenUD5;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class Equipo {
+
+    private static final int PROBLEMAS_RESUELTOS = 0;
+    private static final int PUNTUACION = 0;
+
+    private String nombreEquipo;
+    ArrayList<String> listaMiembros = new ArrayList<>();
+    private int problemasResueltos;
+    private int puntuacion;
+
+    public Equipo(String nombreEquipo) {
+        this.nombreEquipo = nombreEquipo;
+        this.problemasResueltos = PROBLEMAS_RESUELTOS;
+        this.puntuacion = PUNTUACION;
+    }
+
+    public String getNombreEquipo() {
+        return nombreEquipo;
+    }
+
+    public void setNombreEquipo(String nombreEquipo) {
+        this.nombreEquipo = nombreEquipo;
+    }
+
+    public ArrayList<String> getListaMiembros() {
+        return listaMiembros;
+    }
+
+    public void setListaMiembros(ArrayList<String> listaMiembros) {
+        this.listaMiembros = listaMiembros;
+    }
+
+    public int getProblemasResueltos() {
+        return problemasResueltos;
+    }
+
+    public void setProblemasResueltos(int problemasResueltos) {
+        this.problemasResueltos = problemasResueltos;
+    }
+
+    public int getPuntuacion() {
+        return puntuacion;
+    }
+
+    public void setPuntuacion(int puntuacion) {
+        this.puntuacion = puntuacion;
+    }
+
+    public void calcularPuntuacion() {
+
+    }
+
+
+
+    // Insertar miembros mediante una lista de tipo string comprobando que no se exceda la lista de 4 miembros por equipo
+    public void insertarMiembros(String ... nombres) {
+        if (comprobarCantidadMiembrosEquipo() + nombres.length > 4){
+            System.out.println("ERROR: " + Arrays.toString(Arrays.stream(nombres).toArray()) + " no pueden ser inscritos en el equipo " + nombreEquipo + ", porque solo puede haber 4 miembros por equipo y ya están inscritos:  " + listaMiembros);
+        }else {
+            listaMiembros.addAll(Arrays.asList(nombres));
+            System.out.println(Arrays.toString(Arrays.stream(nombres).toArray()) + " añadidos al equipo " + nombreEquipo + "...");
+        }
+
+    }
+
+    // Metodo para devolver la longitud de la lista de miembros
+    public int comprobarCantidadMiembrosEquipo(){
+        return listaMiembros.size();
+    }
+
+
+    @Override
+    public String toString() {
+        return "Equipo{" +
+                "nombreEquipo='" + nombreEquipo + '\'' +
+                ", listaMiembros=" + listaMiembros +
+                ", problemasResueltos=" + problemasResueltos +
+                ", puntuacion=" + puntuacion +
+                '}';
+    }
+
 }
