cc934519178285f4d748fb2f6d8276ab98e77d6d
IVÁN FUSTER SIMARRO <ifussim@outlook.com>
2025-04-01 19:14:46 +0200

    .

---
+++ src/main/java/com/poo/Patri/Main.java
@@ -0,0 +1,31 @@
+package com.poo.Patri;
+
+import java.util.Stack;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+
+        Stack<String> pila = new Stack<>();
+
+        pila.push("tarta de queso");
+        pila.push("cafe");
+        pila.push("trifásico");
+
+        System.out.println(pila.peek());
+
+        System.out.println();
+        for (String postre : pila){
+            System.out.println(postre);
+        }
+
+        System.out.println();
+        while(!pila.isEmpty()){
+            System.out.println(pila.pop());
+            
+        }
+
+    }
+}
