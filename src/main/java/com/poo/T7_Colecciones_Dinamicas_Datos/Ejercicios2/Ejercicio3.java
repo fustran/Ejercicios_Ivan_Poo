ee561e88be7e0bdb4af86cc76413896d7730010d
fustran <ifussim@outlook.com>
2025-04-11 17:13:39 +0200

    ...

--- src/main/java/com/poo/T7_Colecciones_Dinamicas_Datos/Ejercicios2/Ejercicio3.java
+++ src/main/java/com/poo/T7_Colecciones_Dinamicas_Datos/Ejercicios2/Ejercicio3.java
@@ -1,4 +1,6 @@
 package com.poo.T7_Colecciones_Dinamicas_Datos.Ejercicios2;
 
 public class Ejercicio3 {
+
+
 }
--- src/main/java/com/poo/T7_Colecciones_Dinamicas_Datos/Expresiones_Lambda/FiltroProducto.java
+++ src/main/java/com/poo/T7_Colecciones_Dinamicas_Datos/Expresiones_Lambda/FiltroProducto.java
@@ -5,5 +5,4 @@ public interface FiltroProducto {
 
     boolean filtrar (Producto producto);
 
-
 }
